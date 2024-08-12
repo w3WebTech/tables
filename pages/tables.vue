33a82b4d64e962e8be07a8df9c3090cb131c0700
w3WebTech <164163622+w3WebTech@users.noreply.github.com>
2024-08-12 13:00:31 +0530

    fix

--- pages/tables.vue
+++ pages/tables.vue
@@ -1,16 +1,43 @@
-<script setup>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
 import DemoSimpleTableDensity from '@/views/pages/tables/DemoSimpleTableDensity.vue'
+const currentTab = ref(0)
+console.log(currentTab.value, 'currentT')
 </script>
 
 <template>
-  <VRow>
-    <VCol
-      cols="12"
-      style="z-index: 1000"
+  <VCard>
+    <VTabs
+      v-model:active="currentTab"
+      class="v-tabs-pill"
     >
-      <VCard>
+      <VTab>Tab One</VTab>
+      <VTab>Tab Two</VTab>
+      <VTab>Tab Three</VTab>
+      <VTab>Tab Four</VTab>
+      <VTab>Tab Five</VTab>
+    </VTabs>
+
+    <VWindow
+      v-model:active="currentTab"
+      class="mt-5"
+    >
+      <VWindowItem
+        v-for="item in 5"
+        :key="item"
+      >
         <DemoSimpleTableDensity />
-      </VCard>
-    </VCol>
-  </VRow>
+      </VWindowItem>
+    </VWindow>
+     
+  </VCard>
 </template>
+<style lang="scss" scoped>
+.v-btn {
+  padding-left: 40px !important;
+  padding-right: 40px !important;
+}
+</style>
+
+
